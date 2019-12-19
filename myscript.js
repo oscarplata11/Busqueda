47b5d544cfe5da3c74672ef540fb2675f62ef023
unknown <oscarplata1157@gmail.com>
2019-12-19 11:10:05 +0100

    Primera versión: Proyecto Búsqueda

---
+++ myscript.js
@@ -0,0 +1,20 @@
+function busqueda(){
+    var palabraBuscada=document.getElementById("buscador").value;
+
+     if(palabraBuscada==""){
+        document.getElementById("muestraResultado").value="Debe escribir una palabra a buscar";
+    }
+    else{
+        if(palabraBuscada=="azul" || palabraBuscada=="AZUL" || palabraBuscada=="Azul" || palabraBuscada=="verde" || palabraBuscada=="VERDE" || palabraBuscada=="Verde" || palabraBuscada=="rojo" || palabraBuscada=="ROJO" || palabraBuscada=="Rojo"){
+            document.getElementById("muestraResultado").value="Palabra encontrada";
+        }
+            else{
+                document.getElementById("muestraResultado").value="Palabra NO encontrada";
+            }
+    }
+
+}
+
+function limpiar(){
+    window.close();
+}
